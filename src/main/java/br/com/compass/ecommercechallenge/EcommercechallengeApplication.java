f53cce2f80ce48f13f73fda403f9ac61424de26a
augusto <augustotm5@gmail.com>
2025-05-16 20:05:29 -0300

    First commit

---
+++ src/main/java/br/com/compass/ecommercechallenge/EcommercechallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.compass.ecommercechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercechallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercechallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/compass/ecommercechallenge/EcommercechallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compass.ecommercechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercechallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
