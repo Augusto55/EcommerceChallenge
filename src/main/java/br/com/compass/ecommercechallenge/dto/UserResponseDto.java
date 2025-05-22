f9c05b86cad43476319fc56ddd23259222d6d992
Augusto55 <85200864+Augusto55@users.noreply.github.com>
2025-05-22 12:43:45 -0300

    refactor: refactor UserResponseDto

--- src/main/java/br/com/compass/ecommercechallenge/dto/UserResponseDto.java
+++ src/main/java/br/com/compass/ecommercechallenge/dto/UserResponseDto.java
@@ -2,11 +2,18 @@ package br.com.compass.ecommercechallenge.dto;
 
 import br.com.compass.ecommercechallenge.model.Cart;
 import br.com.compass.ecommercechallenge.model.Order;
+import br.com.compass.ecommercechallenge.model.UserTypeEnum;
 
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.UUID;
 
 public record UserResponseDto(UUID id, String name, String email,
+                              Boolean active,
+                              UserTypeEnum userType,
+                              Timestamp createdAt,
+                              Timestamp updatedAt,
                               List<Order> orders,
-                              Cart shoppingCart) {
+                              Cart shoppingCart
+                              ) {
 }
